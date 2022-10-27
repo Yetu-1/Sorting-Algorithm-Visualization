3b9ac5b836e2ef5130a80a183c0ccaad03c4a9c8
David-Salihu <davidsalihu19@gmail.com>
2022-10-27 16:58:53 +0100

    center Bars to the middle of the screen
    Add order selection

--- Sorting Algorithm/Sorting Algorithm/main.cpp
+++ Sorting Algorithm/Sorting Algorithm/main.cpp
@@ -26,48 +26,61 @@ class Sorter : public olc::PixelGameEngine {
             
             return true;
         }
-    
-        int temp;
-        int i = 0, j = 0;
-    
         bool OnUserUpdate(float fElapsedTime) override {
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
-            sortArray();
+            sortArray(false);
             return true;
         }
         
         void DrawArray()
         {
-            int j = 0;
+            int j = 20;
             
             for(auto &i : array)
             {
-                DrawLine(j, 50, j, 50 - i, olc::CYAN);
+                DrawLine(j, 80, j, 80 - i, olc::CYAN);
                 j += 2;
             }
         }
     
-        void sortArray()
+        void sortArray(bool order)
         {
-            if(j < 39 - i)
+            static int idx = 0, j = 0;
+            static bool status = false;
+            int temp;
+            
+            if(status == false)
             {
-                if(array[j] > array[j + 1])
+                if(j < 39 - idx)
                 {
-                     Clear(olc::WHITE);
-                     int temp;
-                     temp=array[j];
-                     array[j]=array[j + 1];
-                     array[j + 1]=temp;
-                     
-                     DrawArray();
+                    if (order)
+                        order = array[j] > array[j + 1];
+                    else
+                        order = array[j] < array[j + 1];
+                    
+                    if(order)
+                    {
+                        Clear(olc::WHITE);
+                        int temp;
+                        temp=array[j];
+                        array[j]=array[j + 1];
+                        array[j + 1] = temp;
+                        DrawArray();
+                    }
+                    j++;
                 }
-                j++;
-            }
-            else{
-                j = 0;
-                i++;
-                if (i == 40)
-                    i = 0;
+                else{
+                    if (idx == 40)
+                    {
+                        status = true;
+                        idx = 0;
+                    }
+                    else
+                    {
+                        j = 0;
+                        idx++;
+                    }
+                }
             }
         }
         
@@ -77,7 +90,7 @@ class Sorter : public olc::PixelGameEngine {
 
 int main(int argc, char const *argv[]) {
 	Sorter demo;
-	if (demo.Construct(100, 50, 10, 10))
+	if (demo.Construct(120, 80, 10, 10))
 		demo.Start();
 
 	return 0;
